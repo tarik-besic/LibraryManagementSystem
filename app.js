f8890b3a3c8f130331ef07436ab05edfed2d460d
Tarik Besic <tarik_besic@live.com>
2021-08-19 13:42:45 +0200

    Upload file added...No Functionality yet

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express=require('express')
 const app=express()
-
+const path=require("path")
 const data=require("./data")
-
+const XLSX=require("xlsx")
 const tare=data.osobe.filter(person=>person.class=="IV-a")
-console.log(tare)
+
+
+
+
+
 app.get('/',(req,res)=>{
-    
-res.send(tare)
+res.sendFile(path.resolve(__dirname,"./index.html"))
 }
 )
+app.get('/upload',(req,res)=>{
+
+
+
+})
+
 
 app.listen(5000,()=>{console.log("Server is listening on port 5000...")})
--- data.js
+++ data.js
@@ -1,29 +1,30 @@
 persons=[
     {
+        id:1,
         name:"Tarik Besic",
         class:"IV-a",
         email:"tarik_besic@live.com",
         telephoneNumber:"062110917"
     },
-    {
+    {   id:2,
         name:"Ensar Dzafo",
         class:"IV-a",
         email:"ensar_dzafo@live.com",
         telephoneNumber:"062810117"
     },
-    {
+    {   id:3,
         name:"Amar Silajdzic",
         class:"IV-a",
         email:"amarSila@live.com",
         telephoneNumber:"066110517"
     },
-    {
+    {   id:4,
         name:"Benjamin Salkic",
         class:"III-a",
         email:"Benjaminsalko@live.com",
         telephoneNumber:"062130117"
     },
-    {
+    {   id:5,
         name:"Evvel Kamenjas",
         class:"IV-a",
         email:"evvelKamo@live.com",
