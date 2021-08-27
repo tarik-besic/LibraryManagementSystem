4faf7bae795832fb36a78402dc7f132b999bfdc9
Tarik Besic <tarik_besic@live.com>
2021-08-27 14:49:22 +0200

    Added database,made small parts of UI,added functionality to fill tables in my page with data from file uploaded by user

--- app.js
+++ app.js
@@ -1,29 +1,39 @@
 const express = require('express')
-const app = express()
 const path = require("path")
-const data = require("./data")
 const XLSX = require("xlsx")
-const tare = data.osobe.filter(person => person.class == "IV-a")
 const multer = require('multer')
 const mongoose = require('mongoose');
 const User = require("./models/user")
+const fs=require('fs');
+
+const app = express()
 
 //connect to mongoDB
-const dbURI = "mongodb+srv://tarik-besic:Parkourislife2424@cluster0.pk5um.mongodb.net/user-data?retryWrites=true&w=majority"
+const dbURI = "mongodb+srv://tarik-besic:{password}@cluster0.pk5um.mongodb.net/user-data?retryWrites=true&w=majority"
 mongoose.connect(dbURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then((result) => console.log("Database connected")).catch((err) => console.log(err)) //ovo je async task pa mogu nastavit sa .then()
 
 app.use(express.json())
+app.use(express.static('public'))
+app.set('view engine','ejs');
 
 const storage = multer.diskStorage({ //telling multer where to store the file
+     
     destination: function (req, file, cb) {
-        cb(null, "public")
+    
+         cb(null, "public");
+          
     },
     filename: function (req, file, cb) {
-        const parts = file.mimetype.split("/");
-        cb(null, `${file.fieldname}-Tarik.xlsx`) //file name will always be static!
+        const parts = file.originalname.split(".");
+        let first=parts.pop()
+        if(first=='xlsx'||first=='xls'||first=='ods')  //file will only be added to public if its correct extension
+        
+        cb(null, `uploadedFile.xlsx`) //file name will always be static uploadedFile.xlsx
+        else 
+        console.log("WRONG FILE UPLOADED")
     }
 })
 
@@ -32,56 +42,93 @@ const upload = multer({
 });
 
 
-
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./index.html"))
-
-    console.log("situacija")
+    arrayOfUsers=read()
+    res.render('index',{arrayOfUsers});
 })
 
-app.get('/add-user', (req, res) => {
-    const user = new User({
-        name: "TARIK",
-        email: "nemamEmail@gmail.com"
-    })
-
-    user.save().then((result) => {
+app.get('/adduser', (req, res) => {
+    res.render('adduser');
+     /*  user.save().then((result) => {
         res.send(result)
     }).catch((err) => {
         console.log(err)
-    })
-
+    })*/
 })
 
 
 app.post("/upload", upload.single("upload"), (req, res) => {
-    const workbook = XLSX.readFile("./public/upload-Tarik.xlsx")
-    let worksheet = workbook.Sheets[workbook.SheetNames[0]];
-
-    let users = []
-    let user = {}
-    for (cell in worksheet) {
-        const cellAsString = cell.toString();
-        //if(cellAsString[1]=='1') continue
-        if (cellAsString[0] === 'A') {
-            user.name = worksheet[cell].v
-        }
-        if (cellAsString[0] === 'B') {
-            user.email = worksheet[cell].v
-            users.push(user)
-            user = {}
-        }
-    }
-
-    users.shift() //droping "name" and "email" from array 
-
-    console.log(users);
-
+    
+    arrayOfUsers=read()
+    res.render('index',{arrayOfUsers});
 
 })
+app.post("/removefile", (req, res) => {
+    
+    const path = './public/uploadedFile.xlsx'
+    
 
+    if(fs.existsSync("./public/uploadedFile.xlsx"))  //it was spamming my console that file was already deleted...that's why I put to check if file exists to delete it then
+    fs.unlinkSync(path);
+
+    
+      
+    
+    
+        arrayOfUsers=read()
+        res.render('index',{arrayOfUsers});
+    
+
+})
 
 
 app.listen(5000, () => {
     console.log("Server is listening on port 5000...")
 })
+
+
+
+
+function read()
+{
+    let readThisFile;
+    const path=(fs.existsSync("./public/uploadedFile.xlsx"))? readThisFile='./public/uploadedFile.xlsx' : readThisFile='./public/emptyFile.xlsx'
+    const workbook = XLSX.readFile(readThisFile)
+    
+    
+    let worksheet = workbook.Sheets[workbook.SheetNames[0]];
+
+    let itUsers = []
+    let basicUsers = []
+    let hairUsers = []
+    let user = {}
+
+    let classwork=0;  //variable for determaning to which array to push users to...eg classwork=0 means itUsers.push(user)...classwork=1 means basicUsers.push(user)
+    for (cell in worksheet) {
+        const cellAsString = cell.toString();
+        //if(cellAsString[1]=='1') continue
+        if (cellAsString[0] === 'A') {
+            
+            if(worksheet[cell].v=="ime")continue;
+            if(worksheet[cell].v=="Opca gimnazija") classwork=1;
+            if(worksheet[cell].v=="frizer") classwork=2;
+            user.name = worksheet[cell].v
+        }
+        if (cellAsString[0] === 'B') {
+            
+            if(worksheet[cell].v=="email")continue;
+            user.email = worksheet[cell].v
+            switch (classwork) {
+                case 0:itUsers.push(user); break;
+
+                case 1:basicUsers.push(user);   break;
+                
+                case 2:hairUsers.push(user); break;
+            }
+            
+            user = {}
+        }
+    }
+
+    return [itUsers,basicUsers,hairUsers]
+}
