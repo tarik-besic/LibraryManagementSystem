6b9dc218c1e6af53d79359547ad5b574595d17cf
Tarik Besic <tarik_besic@live.com>
2022-01-01 20:27:49 +0100

    made issued date and returned date work

--- public/javascript/returnBookJquery.js
+++ public/javascript/returnBookJquery.js
@@ -1,9 +1,10 @@
 const validateDate=(issuedDate,returnedDate)=>{
+    console.log("I:"+issuedDate)
+    console.log("R:"+returnedDate);
     if(returnedDate=="")
         return false;
-    issuedDate=issuedDate.split("/");
 
-    let date1=new Date(issuedDate[2]+"-"+issuedDate[0]+"-"+issuedDate[1]);
+    let date1=new Date(issuedDate);
     let date2=new Date(returnedDate);
 
     return date1<=date2;
@@ -19,9 +20,10 @@ $(document).on('click','.btn_return',function(event){
         email: tbl_row.find("#user").data("email"),
         class:tbl_row.find("#userClass").text().trim(),
         book:tbl_row.find("#userBook").text().trim(),
-        issuedDate:tbl_row.find("#userIssuedDate").text().trim(),
+        issuedDate:tbl_row.find("#userIssuedDate").data("issueddate"),
         returnedDate:tbl_row.find("#userInputDate").val()
     }
+    console.log(obj.issuedDate)
     if(validateDate(obj.issuedDate,obj.returnedDate))
         $.ajax({
             type:"delete",
