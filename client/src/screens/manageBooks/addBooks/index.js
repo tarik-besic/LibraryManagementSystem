749c473a66b26b15b64f5526f7d8af9a01d9c9bb
Tarik Besic <tarik_besic@live.com>
2022-04-23 05:34:28 +0200

    add books screen, small changes

--- client/src/screens/manageBooks/addBooks/index.js
+++ client/src/screens/manageBooks/addBooks/index.js
@@ -1,8 +1,145 @@
-import React from 'react'
-
+import React, { useEffect, useRef, useState } from 'react'
+import CategoryApi from "../../../api/category/index";
+import BookApi from "../../../api/books/issuedBooks/index";
 const AddBooks = () => {
+  const categories = useRef([])
+  const [data, setData] = useState({
+    name: "",
+    author: "",
+    category: "",
+    quantityAll: 0,
+    quantityFree: 0,
+    isbn: ""
+
+  })
+
+  useEffect(() => {
+    const getCategories = async () => {
+      const req = await CategoryApi.getCategories()
+      categories.current = req.data.arrayOfCategories.map(category => category.name);
+      setData({
+        ...data,
+        category: categories.current[0]
+      })
+    }
+    getCategories()
+  }, [])
+
   return (
-    <div>AddBooks</div>
+    <div className='screen addBooksScreen'>
+      <div className="contentWrapper">
+        <h2>Add a new book</h2>
+        <form>
+          <div className="form">
+            <div className="row">
+              <div className="text">
+                Book name
+              </div>
+              <input type="text"
+                onChange={(e) => {
+                  setData({
+                    ...data,
+                    name: e.target.value
+                  })
+                }}
+                value={data.name}
+              />
+            </div>
+            <div className="row">
+              <div className="text">
+                Author
+              </div>
+              <input type="text"
+                value={data.author}
+                onChange={(e) => {
+                  setData({
+                    ...data,
+                    author: e.target.value
+                  })
+                }} />
+            </div>
+            <div className="row">
+              <div className="text">
+                Category
+              </div>
+              <select id="data" onChange={(e) => { setData({ ...data, category: e.target.value }) }}  >
+                {categories.current.map((category, i) => {
+                  return <option value={category} key={i}>{category}</option>
+                })}
+              </select>
+            </div>
+            <div className="row">
+              <div className="text">
+                Book Quantity
+              </div>
+              <input type="text"
+                value={data.quantityAll}
+                onChange={(e) => {
+                  setData({
+                    ...data,
+                    quantityAll: e.target.value,
+                    quantityFree: e.target.value
+                  })
+                }} />
+            </div>
+            <div className="row">
+              <div className="text">
+                isbn
+              </div>
+              <input type="text"
+                value={data.isbn}
+                onChange={(e) => {
+                  setData({
+                    ...data,
+                    isbn: e.target.value
+                  })
+                }} />
+            </div>
+            <div className="btn" onClick={() => {
+
+              if (data.category === "") {
+                console.log("Stavljam", categories[0])
+                setData({
+                  ...data,
+                  category: categories[0]
+                })
+                //empty
+                alert(data.category)
+              }
+
+              //validation
+              if (data.title === "" || data.author === "" || data.isbn === "") {
+                alert("Please fill out all fields");
+                return;
+              }
+              if (!Number(data.quantityAll) || !data.quantityAll || data.quantityAll == "") {
+
+                alert("Please insert Number in Quantity input");
+                return;
+              }
+              BookApi.addBook(data).then(() => {
+                alert("Book added");
+                setData({
+                  category: categories.current[0],
+                  name: "",
+                  author: "",
+                  quantityAll: 0,
+                  quantityFree: 0,
+                  isbn: ""
+                })
+              }).catch((err) => {
+                console.log(err);
+                alert("Error...")
+              }
+              )
+
+            }}>
+              Submit
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
--- client/src/screens/manageBooks/editBooks/index.js
+++ client/src/screens/manageBooks/editBooks/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Table from '../../../components/table/index';
-import IssuedBooksApi from "../../../api/books/issuedBooks/index";
+import BookApi from "../../../api/books/issuedBooks/index";
 import editIcon from "../../../assets/images/icons/edit-icon.png"
 import deleteIcon from "../../../assets/images/icons/delete-icon.png"
 import BooksModal from '../../../components/Modal/booksModal';
@@ -12,25 +12,16 @@ const EditBooks = () => {
   const [modalData, setModalData] = useState(null)
   useEffect(() => {
     const fetchdata = async () => {
-
-      // let resp = await fetch("http://localhost:5000/books/");
-      const resp = IssuedBooksApi.getBooks().then((data) => {
+      BookApi.getBooks().then((data) => {
         setData(data.data.books);
       })
-      // const data = await resp.json();
-
-      // setData(data.books);
     }
     fetchdata();
   }, [])
-  useEffect(() => {
-    console.log("data");
-    console.log(data);
-  }, [data])
 
   return (
     <div className='screen edit-books-screen'>
-      <div className='content-wrapper'>
+      <div className='contentWrapper'>
         <h2>Edit Books</h2>
         <Table
           data={data}
@@ -45,7 +36,7 @@ const EditBooks = () => {
                 onClick: () => { console.log("Sort by author name") }
               },
               quantityFree: {
-                name: "Books avalible",
+                name: "Books available",
                 onClick: () => { console.log("Sort by author name") }
               },
               quantityAll: {
@@ -72,7 +63,7 @@ const EditBooks = () => {
           {
             icon: deleteIcon,
             onClick: (person) => {
-              IssuedBooksApi.deleteBook(person._id).then(() => {
+              BookApi.deleteBook(person._id).then(() => {
                 setData(data.filter((data) => { return data._id !== person._id }))
               })
             }
