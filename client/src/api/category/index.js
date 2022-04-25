d6e0164923ecb5d30abf8962e06d1018a5fd26db
Tarik Besic <tarik_besic@live.com>
2022-04-25 19:33:29 +0200

    delete and update functions to category

--- client/src/api/category/index.js
+++ client/src/api/category/index.js
@@ -3,10 +3,16 @@ import Request from "../index";
 class CategoryApi extends Request{
 
     static addCategory(category){
-        return Request.post("/category",{category});
+        return Request.post("/category",category);
     }
     static getCategories(category){
         return Request.get("/category");
+    }
+    static updateCategory(category){
+        return Request.patch("/category",category);
+    }
+    static deleteCategory(category){
+        return Request.delete("/category",category);
     }
 }
 export default CategoryApi;
