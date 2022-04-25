71645ae7e31785b379a2ee9f14323baada31ee2f
Tarik Besic <tarik_besic@live.com>
2022-04-25 19:31:17 +0200

    controller for deleting category made

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -1,78 +1,68 @@
-const Category=require('../models/category');
+const Category = require('../models/category');
 
-const getCategories=async(req,res)=>
-{
+const getCategories = async (req, res) => {
     let arrayOfCategories;
-try {
-    arrayOfCategories=await Category.find({});
-    res.status(200).json({arrayOfCategories})
-    }
-catch (error) {
-    console.log(error);
-    res.status(501).json({
-        msg:"Some problem while fetching categories from database"
-    })
-}
-}
-
-const postCategory=async(req,res)=>{
-    
-    let name=req.body.name;
-
-    if(name)  //triming only spaces before the name of category and after...eg:"  romance  "=>"romance"
-        name=name.trim();
-        
     try {
-        let category=new Category({
-            name:name
+        arrayOfCategories = await Category.find({});
+        res.status(200).json({ arrayOfCategories })
+    }
+    catch (error) {
+        console.log(error);
+        res.status(501).json({
+            msg: "Some problem while fetching categories from database"
+        })
+    }
+}
+
+const postCategory = async (req, res) => {
+
+    console.log(req.body)
+    try {
+        const category = new Category({
+            name: req.body.name
         })
 
-    await category.save();
-    res.status(200).json(category);
+        await category.save();
+        res.status(200).json({ category });
     } catch (error) {
         console.log(error);
+        res.status(400).json({ msg: "Error while creating category" })
     }
 
 }
 
-const updateCategory=async(req,res)=>{
-    try{
-        const result=await Category.findByIdAndUpdate({_id:req.body._id},{name:req.body.name});
-        if(result){
-            console.log("radim i ovo1")
-            res.status(200).json({msg:"Category updated"})
-        }
-        else{
-            console.log("radim i ovo2")
-            res.status(404).json({msg:"Category not found"})
-
-        }
-    }catch(err){
-        console.log("radim i ovo3")
-        console.log(err);
-        res.status(400).json({err})
-    }
-}
-
-const deleteCategory=async(req,res)=>{
-    let name=req.body.name;  
-    let result;
+const updateCategory = async (req, res) => {
     try {
-        result=await Category.findOneAndDelete({name:name});
-    } catch (error) {
-        console.log(error);
-    }
+        const result = await Category.findByIdAndUpdate({ _id: req.body._id }, { name: req.body.name });
+        if (result) {
+            res.status(200).json({ msg: "Category updated" })
+        }
+        else {
+            res.status(404).json({ msg: "Category not found" })
 
-    if(result)
-    {
-        res.status(200).json(result)
-    }
-    else 
-    res.status(401).json({msg:"Cannot find the category"});
+        }
+    } catch (err) {
 
+        console.log(err);
+        res.status(400).json({ err })
+    }
 }
 
-module.exports={
+const deleteCategory = async (req, res) => {
+
+    const category = req.body;
+    try {
+        const result = await Category.findByIdAndDelete({ _id: category._id })
+        if (result)
+            res.status(200).json({ msg: "Category deleted" })
+        else
+            res.status(404).json({ msg: "Category not found" })
+    } catch (error) {
+        res.status(500).json({err});
+    }
+}
+
+module.exports = {
     getCategories,
     postCategory,
     updateCategory,
